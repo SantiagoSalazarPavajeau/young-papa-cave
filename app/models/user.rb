5c46c90ca580cbb29d98f0f8908d0a2d8dcfc558
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-03-26 17:29:42 +0100

    user authentication has secure password and bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ApplicationRecord
     has_many :projects
     has_many :hobbies, through: :projects
+    has_secure_password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # before_create :email_user_about_signup
 
-    private
+    # private
 
     # def email_user_about_signup
     #     UserMailer.with(user: @user).welcome_email.deliver_later

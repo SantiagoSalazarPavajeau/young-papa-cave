39627d72a32f22110f5442c6ba2d9cedb8c7ca85
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-03-26 17:40:41 +0100

    add password_digest on users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/signup', to: 'users#new'
+
   resources :projects
   resources :hobbies
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_122321) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

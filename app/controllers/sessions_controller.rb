10dc3c8c3020f597ac8c4564e705f092e85981d0
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-07 16:16:31 +0200

    add sessions login/create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,24 @@ class SessionsController < ApplicationController
 
     def welcome
     end
+    
+    def new
+    end
+
+    def create
+        if params[:username] && params[:password]
+            @user = User.find_by(username: params[:username])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.user_id
+                redirect_to @user
+            end
+        else
+            redirect_to root_path :alert "Please enter a correct username and password"
+        end
+
+    end
+
+    def destroy
+
+    end
 end

d11f2b00004c8803c9848bda62daa7b25ff04960
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-08 10:59:54 +0200

    add nested new user project route

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   resources :projects
   resources :hobbies
   resources :users do
-    resources :projects, only: [:show, :index]
+    resources :projects, only: [:show, :index, :new]
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
